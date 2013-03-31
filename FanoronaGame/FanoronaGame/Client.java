cd8a1db5132e095b440e82dad49a38544472b680
mkerins896 <mkerins896@tamu.edu>
2013-03-31 18:34:44 -0500

    I added Client(),  what he posted frm lab

--- FanoronaGame/FanoronaGame/Client.java
+++ FanoronaGame/FanoronaGame/Client.java
@@ -8,7 +8,39 @@ import FanoronaGame.Commands;
 
 public class Client {
 
-	private Client() {}
+/////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////Establishing connection with server side
+    Socket c_socket = null;
+    InputStream c_sockInput;
+    OutputStream c_sockOutput;
+    
+    public Client() {
+        try {
+            c_socket = new Socket("SERVER", 4001);
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host: SERVER.");
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to : SERVER ");
+            System.exit(1);
+        }
+    ///////////////////////////////////////////////////////////////////
+    ////////////////////////Sending and recieving data
+        try {
+            c_sockInput = c_socket.getInputStream();
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the input to : SERVER ");
+            System.exit(1);
+        }
+        try {
+            c_sockOutput = c_socket.getOutputStream();
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the output to : SERVER ");
+            System.exit(1);
+        }
+        
+    }
+////////////////////////////MEGAN WROTE ABOVE CODE.. IT WAS THE ONLINE HELP THAT WAS POSTED///////////////////////////
 
 	public static void main(String[] args) {
 		String host = (args.length < 1) ? null : args[0];
@@ -21,7 +53,7 @@ public class Client {
 			String url = "//localhost/SERVER";
 			Server remoteObject = (Server) Naming.lookup(url);
 			System.out.println("Got remote object");
-			//System.out.println(" 1 + 2 = " + remoteObject.sum(1, 2));
+
 			Registry registry = LocateRegistry.getRegistry(host);
 			Commands stub = (Commands) registry.lookup("Commands");
 			String response = stub.sendCommand();
