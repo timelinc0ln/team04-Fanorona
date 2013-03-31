982002d4c17d0c315af8017130c13de182a1e8a7
mkerins896 <mkerins896@tamu.edu>
2013-03-31 18:30:55 -0500

    Added client server options in menu thing
    they don't work, but the basic structure is there. somewhat

--- FanoronaGame/FanoronaGame/FanoronaGame.java
+++ FanoronaGame/FanoronaGame/FanoronaGame.java
@@ -23,6 +23,7 @@ public class FanoronaGame extends JPanel implements ActionListener {
 			exit = new JMenuItem("Exit");
 	// New Game buttons
 	Object[] options = { "Easy", "Medium", "Hard" };
+	Object[] remote = { "Human vs. CPU", "Client Server" };
 
 	// Buttons for Menus
 	JButton pve = new JButton("Player vs CPU"), back = new JButton("exit");
@@ -31,7 +32,8 @@ public class FanoronaGame extends JPanel implements ActionListener {
 	// Panels for Graphic interface
 	JPanel newGamePanel = new JPanel(), northPanel = new JPanel(),
 			southPanel = new JPanel(), topPanel = new JPanel(),
-			bottomPanel = new JPanel(), playingFieldPanel = new JPanel();
+			bottomPanel = new JPanel();
+	static JPanel playingFieldPanel = new JPanel(); //static so it can be accessed outside this class
 	DrawBoard visibleBoard = new DrawBoard();
 	JLabel gameTitle = new JLabel("Fanorona");
 	//JTextArea text = new JTextArea();
@@ -167,7 +169,7 @@ public class FanoronaGame extends JPanel implements ActionListener {
 //		});
 
 		playingFieldPanel.add( picLabel );
-		playingFieldPanel.setBounds(77, 70, 770, 390);
+		playingFieldPanel.setBounds(77, 70, 770, 390); //not sure if this is used?
 
 		// Create Black Pieces
 		for (int x = 0; x < 9; x++) {
@@ -185,82 +187,82 @@ public class FanoronaGame extends JPanel implements ActionListener {
 		// Manually set Row 3
 		buttonArray[0][2] = new GamePieces('W');
 		//buttonArray[0][2].setLocation(82, 230);
-		buttonArray[0][2].setAlignmentX(82); //DONT THINK THIS LINE
-		buttonArray[0][2].setAlignmentY(230); //OR THIS ONE ARE NEEDED. BUT IT TELLS YOU WHERE THE PIECES ARE
-		buttonArray[0][2].setBackground(new Color(255, 255, 255));
+		//buttonArray[0][2].setAlignmentX(82); //DONT THINK THIS LINE
+		//buttonArray[0][2].setAlignmentY(230); //OR THIS ONE ARE NEEDED. BUT IT TELLS YOU WHERE THE PIECES ARE
+		buttonArray[0][2].setBackground(new Color(255, 255, 255)); //white
 		buttonArray[0][2].addActionListener(this);
 		buttonArray[0][2].setVisible(true);
 		picLabel. add(buttonArray[0][2]);
 
 		buttonArray[1][2] = new GamePieces('B');
 		//buttonArray[1][2].setLocation(167, 230);
-		buttonArray[1][2].setAlignmentX(167);
-		buttonArray[1][2].setAlignmentY(230);
-		buttonArray[1][2].setBackground(new Color(0, 0, 0));
+		//buttonArray[1][2].setAlignmentX(167);
+		//buttonArray[1][2].setAlignmentY(230);
+		buttonArray[1][2].setBackground(new Color(0, 0, 0)); //black
 		buttonArray[1][2].addActionListener(this);
 		buttonArray[1][2].setVisible(true);
 		picLabel.add(buttonArray[1][2]);
 
 		buttonArray[2][2] = new GamePieces('W');
 		//buttonArray[2][2].setLocation(252, 230);
-		buttonArray[2][2].setAlignmentX(252);
-		buttonArray[2][2].setAlignmentY(230);
+		//buttonArray[2][2].setAlignmentX(252);
+		//buttonArray[2][2].setAlignmentY(230);
 		buttonArray[2][2].setBackground(new Color(255, 255, 255));
 		buttonArray[2][2].addActionListener(this);
 		buttonArray[2][2].setVisible(true);
 		picLabel.add(buttonArray[2][2]);
 
 		buttonArray[3][2] = new GamePieces('B');
-//		buttonArray[3][2].setLocation(167, 230);
-		buttonArray[3][2].setAlignmentX(337);
-		buttonArray[3][2].setAlignmentY(230);
+		//buttonArray[3][2].setLocation(167, 230);
+		//buttonArray[3][2].setAlignmentX(337);
+		//buttonArray[3][2].setAlignmentY(230);
 		buttonArray[3][2].setBackground(new Color(0, 0, 0));
 		buttonArray[3][2].addActionListener(this);
 		buttonArray[3][2].setVisible(true);
 		picLabel.add(buttonArray[3][2]);
 
 		buttonArray[4][2] = new GamePieces('E');
-//		buttonArray[4][2].setLocation(422, 230);
-		buttonArray[4][2].setAlignmentX(422);
-		buttonArray[4][2].setAlignmentY(230);
-		buttonArray[4][2].setBackground(new Color(110,110,110)); //background pic color
-		buttonArray[4][2].setContentAreaFilled(false);
-		buttonArray[4][2].setBorderPainted(false);
-		buttonArray[4][2].addActionListener(this);
+		//buttonArray[4][2].setLocation(422, 230);
+		//buttonArray[4][2].setAlignmentX(422);
+		//buttonArray[4][2].setAlignmentY(230);
+		buttonArray[4][2].setBackground(new Color(110,110,110)); //background pic color (dark grey)
+		buttonArray[4][2].setContentAreaFilled(false); //trying to make it invisible
+		buttonArray[4][2].setBorderPainted(false);	//but still clickable.. maybe setEnabled(true)
+		buttonArray[4][2].addActionListener(this);	// and have setVisible(false)?
 		//buttonArray[4][2].setVisible(false);
 		picLabel.add(buttonArray[4][2]);
 
 		buttonArray[5][2] = new GamePieces('W');
-//		buttonArray[5][2].setLocation(507, 230);
-		buttonArray[5][2].setAlignmentX(507);
-		buttonArray[5][2].setAlignmentY(230);
+		//buttonArray[5][2].setLocation(507, 230);
+		//buttonArray[5][2].setAlignmentX(507);
+		//buttonArray[5][2].setAlignmentY(230);
 		buttonArray[5][2].setBackground(new Color(255, 255, 255));
 		buttonArray[5][2].addActionListener(this);
 		buttonArray[5][2].setVisible(true);
 		picLabel.add(buttonArray[5][2]);
 
 		buttonArray[6][2] = new GamePieces('B');
-//		buttonArray[6][2].setLocation(592, 230);
-		buttonArray[6][2].setAlignmentX(592);
-		buttonArray[6][2].setAlignmentY(230);
+		//buttonArray[6][2].setLocation(592, 230);
+		//buttonArray[6][2].setAlignmentX(592);
+		//buttonArray[6][2].setAlignmentY(230);
 		buttonArray[6][2].setBackground(new Color(0, 0, 0));
 		buttonArray[6][2].addActionListener(this);
 		buttonArray[6][2].setVisible(true);
 		picLabel.add(buttonArray[6][2]);
 
 		buttonArray[7][2] = new GamePieces('W');
-//		buttonArray[7][2].setLocation(677, 230);
-		buttonArray[7][2].setAlignmentX(677);
-		buttonArray[7][2].setAlignmentY(230);
+		//buttonArray[7][2].setLocation(677, 230);
+		//buttonArray[7][2].setAlignmentX(677);
+		//buttonArray[7][2].setAlignmentY(230);
 		buttonArray[7][2].setBackground(new Color(255, 255, 255));
 		buttonArray[7][2].addActionListener(this);
 		buttonArray[7][2].setVisible(true);
 		picLabel.add(buttonArray[7][2]);
 
 		buttonArray[8][2] = new GamePieces('B');
-//		buttonArray[8][2].setLocation(762, 230);
-		buttonArray[8][2].setAlignmentX(762);
-		buttonArray[8][2].setAlignmentY(230);
+		//buttonArray[8][2].setLocation(762, 230);
+		//buttonArray[8][2].setAlignmentX(762);
+		//buttonArray[8][2].setAlignmentY(230);
 		buttonArray[8][2].setBackground(new Color(0, 0, 0));
 		buttonArray[8][2].addActionListener(this);
 		buttonArray[8][2].setVisible(true);
@@ -270,9 +272,9 @@ public class FanoronaGame extends JPanel implements ActionListener {
 		for(int y = 3; y < 5; y++ ) {
 			for(int x = 0; x < 9; x++) {
 				buttonArray[x][y] = new GamePieces('W');
-//				buttonArray[x][y].setLocation(79+x*85, 70+y*77);
-				buttonArray[x][y].setAlignmentX(82 + x*85);
-				buttonArray[x][y].setAlignmentY(230 + y*77);
+				//buttonArray[x][y].setLocation(79+x*85, 70+y*77);
+				//buttonArray[x][y].setAlignmentX(82 + x*85);
+				//buttonArray[x][y].setAlignmentY(230 + y*77);
 				buttonArray[x][y].setBackground(new Color(255, 255, 255));
 				buttonArray[x][y].addActionListener(this);
 				buttonArray[x][y].setVisible(true);
@@ -290,30 +292,32 @@ public class FanoronaGame extends JPanel implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent click) {
-		Object actionSource = click.getSource();
-		if (actionSource == newGame) {
-			int option = askMessage(
-					"Are you prepared to start a new game vs CPU?"
-							+ " WARNING: All current progress will be lost.\n",
-					"New Game", JOptionPane.YES_NO_OPTION);
+		Object actionSource = click.getSource(); //Get the source of what object is having an action performed
+		if (actionSource == newGame) { //If you click the button New Game
+			int option = JOptionPane.showOptionDialog(newGamePanel,
+								"Would you like to play over a server? Or directly with the CPU?"
+							+ " WARNING: All current progress will be lost.\n", "New Game",
+								JOptionPane.YES_NO_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null, remote, remote[1]);
+//					"Would you like to play over a server? Or directly with the CPU?"
+//							+ " WARNING: All current progress will be lost.\n", "New Game",
+//                                                        JOptionPane.YES_NO_OPTION);
 			if (option == JOptionPane.YES_OPTION) {
-				int playOption = JOptionPane
-						.showOptionDialog(newGamePanel,
-								"Select your difficulty", "Difficulty Level",
+				int playOption = JOptionPane.showOptionDialog(newGamePanel,
+								"You've chosen to play against the CPU\n Select your difficulty", "Difficulty Level",
 								JOptionPane.YES_NO_CANCEL_OPTION,
-								JOptionPane.QUESTION_MESSAGE, null, options,
-								options[2]);
-				if (playOption == JOptionPane.OK_OPTION) {
+								JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+				if (playOption == JOptionPane.OK_OPTION) { //Easy difficutly
+					window.remove(southPanel);
+					window.add(playingFieldPanel, BorderLayout.CENTER);
+					window.validate(); // is validate needed?
+					//window.setVisible(true);
+				} else if (playOption == JOptionPane.NO_OPTION) { //Medium difficutly
 					window.remove(southPanel);
 					window.add(playingFieldPanel, BorderLayout.CENTER);
 					window.validate();
 					//window.setVisible(true);
-				} else if (playOption == JOptionPane.NO_OPTION) {
-					window.remove(southPanel);
-					window.add(playingFieldPanel, BorderLayout.CENTER);
-					window.validate();
-					//window.setVisible(true);
-				} else if (playOption == JOptionPane.CANCEL_OPTION) {
+				} else if (playOption == JOptionPane.CANCEL_OPTION) { //Hard difficulty
 					window.remove(southPanel);
 					window.add(playingFieldPanel, BorderLayout.CENTER);
 					window.validate();
@@ -321,26 +325,61 @@ public class FanoronaGame extends JPanel implements ActionListener {
 				}
 
 			}
-		} else if (actionSource == exit) {
-			int option = askMessage("Are you sure you want to exit?",
-					"Exit Game", JOptionPane.YES_NO_OPTION);
-			if (option == JOptionPane.YES_OPTION)
-				System.exit(0);
-		} else if (actionSource == about) {
+                        if(option == JOptionPane.NO_OPTION) {
+                        	int playerOption = askMessage("You have chosen to play across a server, AI vs. AI \n Continue? \n",
+                                                            "Client Server", JOptionPane.YES_NO_OPTION);
+                            if(playeroption == YES_OPTION) {
+                        	int newOption = JOptionPane.showOptionDialog(newGamePanel,
+								"Ok great, choose the difficulty of the AIs", "Difficulty AI Level",
+								JOptionPane.YES_NO_CANCEL_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+					if (newOption == JOptionPane.OK_OPTION) { //Easy difficutly
+						window.remove(southPanel);
+						window.add(playingFieldPanel, BorderLayout.CENTER);
+						//Server e_srv = new Server(); //this doesn't work
+						//Client e_clt = new Client(); //this doesn't work
+						window.validate(); // is validate needed?
+						//window.setVisible(true);
+					} else if (newOption == JOptionPane.NO_OPTION) { //Medium difficutly
+						window.remove(southPanel);
+						window.add(playingFieldPanel, BorderLayout.CENTER);
+						//Server m_srv = new Server(); //this doesn't work
+						//Client m_clt = new Client(); //this doesn't work
+						window.validate();
+						//window.setVisible(true);
+					} else if (newOption == JOptionPane.CANCEL_OPTION) { //Hard difficulty
+						window.remove(southPanel);
+						window.add(playingFieldPanel, BorderLayout.CENTER);
+						//Server h_srv = new Server(); //this doesn't work
+						//Client h_clt = new Client(); //this doesn't work
+						window.validate();
+						//window.setVisible(true);
+					}
+                            }  
+                                
+                        }
+                }
+                        
+                else if (actionSource == exit) { //If you clicked the Exit button
+                    int exit_option = askMessage("Are you sure you want to exit?",
+                                    "Exit Game", JOptionPane.YES_NO_OPTION);
+                    if (exit_option == JOptionPane.YES_OPTION) //yes == close window. No, do nothing.
+                            System.exit(0);
+                
+		} else if (actionSource == about) { //If you clicked the About button, tells who created it.
 			JOptionPane.showMessageDialog(null,
 					"This Game was created by Megan Kernis, Patrick Casey, and Matt Hacker.\n"
-							+ "Current Version: 0.8\n"
+							+ "Current Version: 0.1\n"
 							+ "Team 04, CSCE 315-501\n", "About",
 					JOptionPane.ERROR_MESSAGE);
-		} else if (actionSource == instructions) {
-			JOptionPane
-					.showMessageDialog(
-							null,
-							"Move your piece toward or way from an enemy piece to capture it.\n"
-									+ "You may only move a piece that will cause the capture of an enemy piece.\n"
-									+ "The Game will continue as long as there are valid move to be made.\n"
-									+ "More instructions to follow...",
-							"Instructions", JOptionPane.ERROR_MESSAGE);
+                        
+		} else if (actionSource == instructions) { //The Instructions button, tells how to play the game and rules
+			JOptionPane.showMessageDialog(null,
+                                        "Move your piece toward or way from an enemy piece to capture it.\n"
+                                                        + "You may only move a piece that will cause the capture of an enemy piece.\n"
+                                                        + "The Game will continue as long as there are valid move to be made.\n"
+                                                        + "More instructions to follow...", "Instructions", 
+                                        JOptionPane.ERROR_MESSAGE);
 		}
 	}
 
@@ -350,5 +389,7 @@ public class FanoronaGame extends JPanel implements ActionListener {
 
 	public static void main(String[] args) throws IOException {
 		FanoronaGame game = new FanoronaGame();
+		//Client client = new Client();
+		//Server server = new Server();
 	}
 }
