6a24edd2351b4357beb742966af9dcba46c40bbd
Patrick Casey <patrickcasey2014@gmail.com>
2013-04-01 15:39:28 -0500

    AI Selects move and executes

--- FanoronaGame/FanoronaGame/FanoronaAI.java
+++ FanoronaGame/FanoronaGame/FanoronaAI.java
@@ -8,20 +8,112 @@ public class FanoronaAI extends Board {
 	char aiColor = 'B'; // defualt is AI player is Black
 	boolean isTurn; 
 
+	public int[] selectDirection(int i, int j, Boolean north, Boolean south, 
+		Boolean east, Boolean west, Boolean southEast, Boolean southWest, 
+		Boolean northEast, Boolean northWest) {
+
+		int coordinates = new int[2];
+		boolean foundMove == false;
+
+		if (north == true) {
+			if (!foundMove)
+			{
+				int moveI = ++i; 
+				int moveJ = j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true; 
+			}
+		}
+		else if (south == true) {
+			if(!foundMove) {
+				int moveI = --i; 
+				int moveJ = j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		else if (east == true) {
+			if(!foundMove) {
+				int moveI = i; 
+				int moveJ = --j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		else if (west == true) {
+			if(!foundMove) {
+				int moveI = i; 
+				int moveJ = ++j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		else if (northEast == true) {
+			if(!foundMove) {
+				int moveI = ++i; 
+				int moveJ = --j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		else if (northWest == true) {
+			if(!foundMove) {
+				int moveI = ++i; 
+				int moveJ = ++j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		else if (southWest == true) {
+			if(!foundMove) {
+				int moveI = --i; 
+				int moveJ = ++j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		else if (southEast == true) {
+			if(!foundMove) {
+				int moveI = --i; 
+				int moveJ = --j;
+				coordinate.add(moveI);
+				coordinates.add(moveJ);
+				foundMove = true;
+			}
+		}
+		return coordinates; 
+	}
 	/**
 	* Function for selecting which piece to move
 	* @param aiColor can be either white or black
 	* depending on what the user selects to play as
 	*/
-	public selectPiece(char aiColor) {
+	public void selectPiece(char aiColor) {
 		// randomly select a piece using X,Y coordinates that has a valid move
 		int size = validMoves.size();
 		int random = 0 + (int)(Math.random() * ((size - 0) + 1));
 		int xPos = validMoves[random].x_index;
 		int yPos = validMoves[random].y_index;
-
-		// perform capture move
-		turn(aiColor, xPos, yPos, moveX, moveY, forward);
+		int[] coordinates = new int[2]; 
+		// Determine what pieces it can capture
+		Boolean north = validMoves[random].north;
+		Boolean northEast = validMoves[random].northEast;
+		Boolean northWest = validMoves[random].northWest;
+		Boolean south = validMoves[random].south;
+		Boolean southEast = validMoves[random].southEast;
+		Boolean southWest = validMoves[random].southWest;
+		Boolean east = validMoves[random].east;
+		Boolean west = validMoves[random].west; 
+		coordinates = selectDirection(); 
+		// perform capture move with basic capture options
+		turn(aiColor, 1, xPos, yPos, coordinates[0], coordinates[1]);
 	}
 
 	/**
